a03147d819aa4345de3b1659c12938fe89e0415f
Jacek <Jacek@BERLIN>
2020-04-13 09:54:00 +0200

    xx

--- HibernateKurs5_Sekcja9_Mapped_Superclass_Inheritance/src/main/java/pl/jaceksysiak/demo/Application_CreateData.java
+++ HibernateKurs5_Sekcja9_Mapped_Superclass_Inheritance/src/main/java/pl/jaceksysiak/demo/Application_CreateData.java
@@ -1,14 +1,12 @@
 package pl.jaceksysiak.demo;
 
-import java.math.BigDecimal;
-import java.util.Date;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import pl.jaceksysiak.demo.entity.AccountType;
-import pl.jaceksysiak.demo.entity.Account;
+import pl.jaceksysiak.demo.entity.Bond;
+import pl.jaceksysiak.demo.entity.Investment;
+import pl.jaceksysiak.demo.entity.Stock;
 
 public class Application_CreateData {
 
@@ -17,8 +15,9 @@ public class Application_CreateData {
 		// create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Account.class)
-								.addAnnotatedClass(AccountType.class)
+								.addAnnotatedClass(Stock.class)
+								.addAnnotatedClass(Investment.class)
+								.addAnnotatedClass(Bond.class)
 								.buildSessionFactory();
 		
 		// create session
@@ -29,16 +28,9 @@ public class Application_CreateData {
 			session.beginTransaction();
 			
 			//Creating the Object 
-			Account account = createNewAccount();
-			account.setAccountType(AccountType.SAVINGS);
 			
 			//Saving the Object to DB		
-			session.save(account);
 			
-			
-			Account dbAccount = (Account) session.get(Account.class, account.getAccountId());
-			System.out.println(dbAccount.getName());
-			System.out.println(dbAccount.getAccountType());
 			
 			// commit transaction
 			session.getTransaction().commit();
@@ -52,20 +44,6 @@ public class Application_CreateData {
 			session.close();			
 			factory.close();
 		}
-	}
-	
-	private static Account createNewAccount() {
-		Account account = new Account();
-		account.setCloseDate(new Date());
-		account.setOpenDate(new Date());
-		account.setCreatedBy("Kevin Bowersox");
-		account.setInitialBalance(new BigDecimal("50.00"));
-		account.setName("Savings Account");
-		account.setCurrentBalance(new BigDecimal("100.00"));
-		account.setLastUpdatedBy("Kevin Bowersox");
-		account.setLastUpdatedDate(new Date());
-		account.setCreatedDate(new Date());
-		return account;
 	}
 	
 
